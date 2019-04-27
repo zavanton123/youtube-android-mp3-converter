cb086a257887e8905d520decb077653d5721381e
zavanton <zavanton@yandex.ru>
2019-04-27 21:29:42 +0300

    intent service is not used

--- app/src/main/java/com/zavanton/youtube_downloader/service/DownloadService.kt
+++ app/src/main/java/com/zavanton/youtube_downloader/service/DownloadService.kt
@@ -1,16 +1,24 @@
 package com.zavanton.youtube_downloader.service
 
 import android.annotation.SuppressLint
-import android.app.IntentService
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.os.IBinder
 import android.support.v4.app.NotificationCompat
 import android.util.Log
 import android.util.SparseArray
 import at.huber.youtubeExtractor.VideoMeta
 import at.huber.youtubeExtractor.YouTubeExtractor
 import at.huber.youtubeExtractor.YtFile
+import com.zavanton.youtube_downloader.ui.MainActivity
 
-class DownloadService : IntentService("DownloadService") {
+
+class DownloadService : Service() {
 
     companion object {
 
@@ -18,26 +26,65 @@ class DownloadService : IntentService("DownloadService") {
         private val FORMAT_TAG = 22
         private val FOREGROUND_NOTIFICATION_ID = 123
         private val NOTIFICATION_CHANNEL_HIGH = "NOTIFICATION_CHANNEL_HIGH"
+        private val NOTIFICATION_CHANNEL_NORMAL = "NOTIFICATION_CHANNEL_NORMAL"
+        private val ACTIVITY_CODE = 234
     }
 
-    override fun onHandleIntent(intent: Intent?) {
-        Log.d("zavanton", "onHandleIntent")
+    override fun onCreate() {
+        super.onCreate()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            setUpNotificationChannel()
+        }
+    }
+
+    private fun setUpNotificationChannel() {
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                NOTIFICATION_CHANNEL_NORMAL,
+                "Normal notification", NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         makeServiceForeground()
         runTask()
+
+        return Service.START_NOT_STICKY
     }
 
     private fun makeServiceForeground() {
         val builder = NotificationCompat.Builder(
             this,
-            NOTIFICATION_CHANNEL_HIGH
+            NOTIFICATION_CHANNEL_NORMAL
         )
 
-        builder.setContentTitle("Foreground service title")
-            .setContentText("Foreground service text")
+        val activityIntent = Intent(this, MainActivity::class.java)
+        activityIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+
+        val pendingIntent = PendingIntent.getActivity(
+            this, ACTIVITY_CODE, activityIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        builder.setContentTitle("Foreground service notification content title")
+            .setContentInfo("Service is running in the foreground.")
+            .setContentIntent(pendingIntent)
+            .setContentText("This is content text")
             .setSmallIcon(android.R.drawable.stat_sys_download)
             .setOngoing(true)
 
+        // Starts foreground
         startForeground(FOREGROUND_NOTIFICATION_ID, builder.build())
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
     }
 
     @SuppressLint("StaticFieldLeak")
@@ -45,7 +92,6 @@ class DownloadService : IntentService("DownloadService") {
 
         object : YouTubeExtractor(this) {
             public override fun onExtractionComplete(ytFiles: SparseArray<YtFile>?, vMeta: VideoMeta) {
-                Log.d("zavanton", "onExtractionComplete")
                 if (ytFiles != null) {
 
                     val url = ytFiles[FORMAT_TAG].url
