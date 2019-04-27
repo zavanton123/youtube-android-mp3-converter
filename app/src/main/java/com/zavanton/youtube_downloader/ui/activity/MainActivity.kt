5e12637432372ffc7a45bec705dfab1e07bf080e
zavanton <zavanton@yandex.ru>
2019-04-27 22:18:14 +0300

    file is downloaded

--- app/src/main/java/com/zavanton/youtube_downloader/ui/activity/MainActivity.kt
+++ app/src/main/java/com/zavanton/youtube_downloader/ui/activity/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.zavanton.youtube_downloader.ui.activity
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import com.tbruyelle.rxpermissions2.RxPermissions
 import com.zavanton.youtube_downloader.R
 import com.zavanton.youtube_downloader.ui.service.DownloadService
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,11 +22,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        val intent = Intent(this, DownloadService::class.java)
 
         fab.setOnClickListener {
-            startService(intent)
+            checkPermissionsAndStartDownload()
         }
+    }
+
+    private fun checkPermissionsAndStartDownload() {
+        val permissions = arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+
+        RxPermissions(this).request(*permissions).subscribe(
+            {
+                Log.d("zavanton", "permissions are granted")
+                val intent = Intent(this, DownloadService::class.java)
+                startService(intent)
+            },
+            {
+                Log.d("zavanton", "permissions are NOT granted")
+            }
+        )
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
