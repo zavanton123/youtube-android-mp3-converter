1162d33708c941ef3a224090c6ba693a1a2695cc
zavanton <zavanton@yandex.ru>
2019-04-28 13:46:38 +0300

    logger added

--- app/src/main/java/com/zavanton/youtube_downloader/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/zavanton/youtube_downloader/ui/splash/SplashActivity.kt
@@ -3,12 +3,23 @@ package com.zavanton.youtube_downloader.ui.splash
 import android.Manifest
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.tbruyelle.rxpermissions2.RxPermissions
 import com.zavanton.youtube_downloader.ui.activity.MainActivity
+import com.zavanton.youtube_downloader.utils.Logger
+import io.reactivex.disposables.CompositeDisposable
 
 class SplashActivity : AppCompatActivity() {
+
+    companion object {
+
+        private val PERMISSIONS = arrayOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+    }
+
+    private val compositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,20 +28,24 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun checkPermissionsAndStartApplication() {
-        val permissions = arrayOf(
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-        )
-
-        RxPermissions(this).request(*permissions).subscribe(
-            { goToMainActivity() },
-            { Log.d("zavanton", "permissions are NOT granted") }
+        compositeDisposable.add(
+            RxPermissions(this).request(*PERMISSIONS).subscribe(
+                { goToMainActivity() },
+                { Logger.d("permissions are NOT granted") }
+            )
         )
     }
 
     private fun goToMainActivity() {
+        Logger.d("goToMainActivity")
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Logger.d("onDestroy")
+        compositeDisposable.dispose()
     }
 }
---
+++ app/src/main/java/com/zavanton/youtube_downloader/utils/Logger.kt
@@ -0,0 +1,10 @@
+package com.zavanton.youtube_downloader.utils
+
+import android.util.Log
+
+object Logger {
+
+    fun d(message: String) {
+        Log.d("zavantondebug", message)
+    }
+}
