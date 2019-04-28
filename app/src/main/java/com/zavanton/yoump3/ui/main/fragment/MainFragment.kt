062eb89b4dc91236581678c8aa238c8c14dc96fd
zavanton <zavanton@yandex.ru>
2019-04-28 19:14:53 +0300

    data binding added

--- app/src/main/java/com/zavanton/yoump3/ui/main/fragment/MainFragment.kt
+++ app/src/main/java/com/zavanton/yoump3/ui/main/fragment/MainFragment.kt
@@ -5,53 +5,55 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.zavanton.yoump3.R
+import com.zavanton.yoump3.databinding.FmtMainBinding
 import com.zavanton.yoump3.ui.service.DownloadService
-import kotlinx.android.synthetic.main.content_main.view.*
-import kotlinx.android.synthetic.main.fmt_main.view.*
 
 class MainFragment : Fragment() {
 
+    private lateinit var bind: FmtMainBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fmt_main, container, false)
+        bind = DataBindingUtil.inflate<FmtMainBinding>(inflater, R.layout.fmt_main, container, false)
 
-        initUI(view)
+        initUI()
 
-        return view
+        return bind.root
     }
 
-    private fun initUI(view: View) {
-        initToolbar(view)
-        showClipboardStatus(view)
-        initFab(view)
+    private fun initUI() {
+        initToolbar()
+        showClipboardStatus()
+        initFab()
     }
 
-    private fun initToolbar(view: View) {
-        view.vToolbar.title = requireContext().getString(R.string.app_name)
+    private fun initToolbar() {
+        bind.vToolbar.title = requireContext().getString(R.string.app_name)
     }
 
-    private fun showClipboardStatus(view: View) {
+    private fun showClipboardStatus() {
         val isEmpty = false
         if (isEmpty) {
-            showEmptyClipboard(view)
+            showEmptyClipboard()
         } else {
-            showFullClipboard(view)
+            showFullClipboard()
         }
     }
 
-    private fun showFullClipboard(view: View) {
-        view.vBox.setImageResource(R.drawable.ic_full_box)
-        view.vClipboardState.text = getString(R.string.clipboard_full)
+    private fun showFullClipboard() {
+        bind.vBox.setImageResource(R.drawable.ic_full_box)
+        bind.vClipboardState.text = getString(R.string.clipboard_full)
     }
 
-    private fun showEmptyClipboard(view: View) {
-        view.vBox.setImageResource(R.drawable.ic_empty_box)
-        view.vClipboardState.text = getString(R.string.clipboard_empty)
+    private fun showEmptyClipboard() {
+        bind.vBox.setImageResource(R.drawable.ic_empty_box)
+        bind.vClipboardState.text = getString(R.string.clipboard_empty)
     }
 
-    private fun initFab(view: View) {
-        view.vFab.setOnClickListener {
+    private fun initFab() {
+        bind.vFab.setOnClickListener {
             startDownloadService()
         }
     }
---
+++ app/src/main/java/com/zavanton/yoump3/ui/main/fragment/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.zavanton.yoump3.ui.main.fragment
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+
+}
