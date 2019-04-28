c3ac80a552c621eef5765982dca6ef445a317c9c
zavanton <zavanton@yandex.ru>
2019-04-28 13:30:36 +0300

    colors changed

---
+++ app/src/main/java/com/zavanton/youtube_downloader/ui/splash/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.zavanton.youtube_downloader.ui.splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+}
