1dabe31a9335a858b2940252adbb28729ea1acf1
zavanton <zavanton@yandex.ru>
2019-04-28 13:39:51 +0300

    permission checking is in splash screen

--- app/src/main/java/com/zavanton/youtube_downloader/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/zavanton/youtube_downloader/ui/splash/SplashActivity.kt
@@ -1,14 +1,36 @@
 package com.zavanton.youtube_downloader.ui.splash
 
+import android.Manifest
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.tbruyelle.rxpermissions2.RxPermissions
+import com.zavanton.youtube_downloader.ui.activity.MainActivity
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
+        checkPermissionsAndStartApplication()
     }
 
+    private fun checkPermissionsAndStartApplication() {
+        val permissions = arrayOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+
+        RxPermissions(this).request(*permissions).subscribe(
+            { goToMainActivity() },
+            { Log.d("zavanton", "permissions are NOT granted") }
+        )
+    }
+
+    private fun goToMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
 }
