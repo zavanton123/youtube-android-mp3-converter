54cde5a9a5eac3670a5d37af369d36db000980b5
zavanton <zavanton@yandex.ru>
2019-05-01 11:32:02 +0300

    appComponent is init in TheApp

--- app/src/main/java/com/zavanton/yoump3/app/TheApp.kt
+++ app/src/main/java/com/zavanton/yoump3/app/TheApp.kt
@@ -5,13 +5,30 @@ import android.util.Log
 import com.github.hiteshsondhi88.libffmpeg.FFmpeg
 import com.github.hiteshsondhi88.libffmpeg.LoadBinaryResponseHandler
 import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegNotSupportedException
+import com.zavanton.yoump3.di.component.AppComponent
+import com.zavanton.yoump3.di.component.DaggerAppComponent
+import com.zavanton.yoump3.di.module.AppModule
 
 class TheApp : Application() {
+
+    companion object {
+
+        private lateinit var appComponent: AppComponent
+
+        fun getAppComponent(): AppComponent = appComponent
+    }
 
     override fun onCreate() {
         super.onCreate()
 
+        initAppComponent()
         initFfmpeg()
+    }
+
+    private fun initAppComponent() {
+        appComponent = DaggerAppComponent.builder()
+            .appModule(AppModule(this))
+            .build()
     }
 
     private fun initFfmpeg() {
