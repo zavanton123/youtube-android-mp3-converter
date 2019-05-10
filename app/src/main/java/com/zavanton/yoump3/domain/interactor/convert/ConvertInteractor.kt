852ae47fa43f10b0fa7ebf25741d1f0720b7145e
zavanton <zavanton@yandex.ru>
2019-05-10 11:23:26 +0300

    conversion logic moved to manager

--- app/src/main/java/com/zavanton/yoump3/domain/interactor/convert/ConvertInteractor.kt
+++ app/src/main/java/com/zavanton/yoump3/domain/interactor/convert/ConvertInteractor.kt
@@ -1,9 +1,7 @@
 package com.zavanton.yoump3.domain.interactor.convert
 
-import com.github.hiteshsondhi88.libffmpeg.ExecuteBinaryResponseHandler
-import com.github.hiteshsondhi88.libffmpeg.FFmpeg
 import com.zavanton.yoump3.di.qualifier.scheduler.IoThreadScheduler
-import com.zavanton.yoump3.utils.Logger
+import com.zavanton.yoump3.domain.model.FfmpegManager
 import io.reactivex.Observable
 import io.reactivex.Scheduler
 import javax.inject.Inject
@@ -11,45 +9,15 @@ import javax.inject.Inject
 class ConvertInteractor
 @Inject
 constructor(
-    private val ffmpeg: FFmpeg,
+    private val ffmpegManager: FfmpegManager,
     @IoThreadScheduler
     private val ioThreadScheduler: Scheduler
 ) : IConvertInteractor {
 
     override fun convertToMp3(videoFile: String, audioFile: String): Observable<String> =
-        Observable.create { emitter ->
-
+        Observable.create {
             ioThreadScheduler.scheduleDirect {
-
-                val commands = arrayOf("-i", videoFile, audioFile)
-
-                ffmpeg.execute(commands, object : ExecuteBinaryResponseHandler() {
-
-                    override fun onStart() {
-                        Logger.d("onStart")
-                    }
-
-                    override fun onProgress(message: String) {
-                        Logger.d("onProgress: $message")
-                        emitter.onNext(message)
-                    }
-
-                    override fun onFailure(message: String) {
-                        val throwable = Throwable(message)
-                        Logger.e("onFailure: $message", throwable)
-                        emitter.onError(throwable)
-                    }
-
-                    override fun onSuccess(message: String) {
-                        Logger.d("onSuccess: $message")
-                        emitter.onNext(message)
-                    }
-
-                    override fun onFinish() {
-                        Logger.d("onFinish")
-                        emitter.onComplete()
-                    }
-                })
+                ffmpegManager.convert(it, arrayOf("-i", videoFile, audioFile))
             }
         }
 }
--- app/src/main/java/com/zavanton/yoump3/domain/model/FfmpegManager.kt
+++ app/src/main/java/com/zavanton/yoump3/domain/model/FfmpegManager.kt
@@ -1,9 +1,12 @@
 package com.zavanton.yoump3.domain.model
 
+import com.github.hiteshsondhi88.libffmpeg.ExecuteBinaryResponseHandler
 import com.github.hiteshsondhi88.libffmpeg.FFmpeg
 import com.github.hiteshsondhi88.libffmpeg.LoadBinaryResponseHandler
+import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegCommandAlreadyRunningException
 import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegNotSupportedException
 import com.zavanton.yoump3.utils.Logger
+import io.reactivex.ObservableEmitter
 import javax.inject.Inject
 
 class FfmpegManager
@@ -32,6 +35,41 @@ constructor(private val ffmpeg: FFmpeg) {
             })
         } catch (exception: FFmpegNotSupportedException) {
             Logger.e("FFmpeg - Error while initializing FFmpeg", exception)
+        }
+    }
+
+    fun convert(emitter: ObservableEmitter<String>, commands: Array<String>) {
+        try {
+            ffmpeg.execute(commands, object : ExecuteBinaryResponseHandler() {
+
+                override fun onStart() {
+                    Logger.d("onStart")
+                }
+
+                override fun onProgress(message: String) {
+                    Logger.d("onProgress: $message")
+                    emitter.onNext(message)
+                }
+
+                override fun onFailure(message: String) {
+                    val throwable = Throwable(message)
+                    Logger.e("onFailure: $message", throwable)
+                    emitter.onError(throwable)
+                }
+
+                override fun onSuccess(message: String) {
+                    Logger.d("onSuccess: $message")
+                    emitter.onNext(message)
+                }
+
+                override fun onFinish() {
+                    Logger.d("onFinish")
+                    emitter.onComplete()
+                }
+            })
+        } catch (exception: FFmpegCommandAlreadyRunningException) {
+            Logger.e("FfmpegManager - FFmpegCommandAlreadyRunningException", exception)
+            emitter.onError(exception)
         }
     }
 }
